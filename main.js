ecd969687b5a68a1f06b2d1351d9942e5ec3dbc3
Lorieanne Teofilo <91080636+loreynn@users.noreply.github.com>
2021-12-15 16:34:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = ' Hi! my name is Lorieanne Teofilo, 19 years old.I write, design and love making arts.\n I am a first year student at Adamson University and at the same time a part time Web Developer at Google.\n I am a dreamer and a hard working studentI strive to learn and explore more about the Information Technology Industry as I expand my knowledge and capabilities through the process. ';
+var txt = ' Hi! my name is Lorieanne Teofilo, 19 years old.I write, design and love making arts.\n I am a first year student at Adamson University and at the same time a part time Web Developer at Google.\n I am a dreamer and a hard working student I strive to learn and explore more about the Information Technology Industry as I expand my knowledge and capabilities through the process. ';
 var speed = 50;
 
 function AboutMe() {
